20190e1a3ce5c22c525830ae64e9a70b60e1d407
Carlo Feniello <carlofeniello@hotmail.it>
2022-03-03 17:12:06 +0100

    Create string.js

---
+++ string.js
@@ -0,0 +1,22 @@
+let s = 'Hello, World!';
+
+s.substring(1,4)
+s.slice(1,4)
+s.split(",")
+
+s.indexOf("1")
+s.lastIndexOf("1")
+
+s.startsWith("Hell") // true
+s.EndsWith("!") // true
+s.include("not") // false
+s.replace("llo", "ya")
+
+s.toLowerCase()
+s.toUpperCase()
+
+s.cartAt(0)
+
+s.trim()
+
+s.concat("!")
