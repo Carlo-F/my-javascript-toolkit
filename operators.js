7dc7db5613e818e0d9c3b70f3a2011b480f6e12b
Carlo Feniello <carlofeniello@hotmail.it>
2022-03-07 16:10:34 +0100

    Update operators.js

--- operators.js
+++ operators.js
@@ -1,5 +1,17 @@
 let data = {x: 1, y: 2);
 
+//postfix increment
+let x = 3;
+y = x++;
+// y = 3
+// x = 4
+            
+//prefix increment
+let a = 2;
+b = ++a;
+// a = 3
+// b = 3
+
 delete data.x;
 typeof data;
 void;
