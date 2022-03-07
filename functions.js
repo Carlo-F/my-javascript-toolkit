365aa48767da422e6edff76c9db0b2c9a687366f
Carlo Feniello <carlofeniello@hotmail.it>
2022-03-07 16:19:54 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -18,3 +18,40 @@ uniqueInteger.counter = 0;
 function uniqueInteger() {
   return uniqueInteger.counter++;
 }
+
+//Closure
+let  uniqueInteger = (function() {
+  let counter = 0;
+  return function() { return counter++; };
+}());
+uniqueInteger();
+uniqueInteger();
+
+//High order function
+function compose(f,g) {
+  return function(...args) {
+    return f.call(this, g.apply(this.args));
+  };
+}
+
+//Memoization
+const memoize = () => {
+  let cache = {};
+  return (n) => {
+    if (n in cache) {
+      console.log('Fetching from cache');
+      return cache[n];
+    }
+    else {
+      console.log('Calculating result');
+      let result = n + 10;
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+const addMemo = memoize();
+addMemo(2); // Calculating result..
+addMemo(3); // Calculating result..
+addMemo(2); // Fetching from cache..
