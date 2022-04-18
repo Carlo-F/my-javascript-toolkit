8755f37c7788c97c750758d69aab40871b577632
Carlo Feniello <carlofeniello@hotmail.it>
2022-04-18 09:44:54 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -3,3 +3,18 @@ const sequence = function*(from,to){
 };
 
 [...sequence(3,5)] // [3,4,5]
+
+function* oneDigitPrimes(){
+    yield 2;
+    yield 3;
+    yield 5;
+    yield 7;
+}
+
+function* sequence(...iterables) {
+    for(let iterable of iterables) {
+        yield* iterable;
+    }
+}
+
+[...sequence("abc", oneDigitPrimes())] // ["a,"b","c",2,3,5,7]
